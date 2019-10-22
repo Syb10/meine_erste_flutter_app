3063a71471ef5954fca2dfcbb8f9feda2e5fedbf
Syb10 <sybillepradella@web.de>
2019-10-22 17:09:34 +0200

    meine Homepage

--- lib/main.dart
+++ lib/main.dart
@@ -10,55 +10,54 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Sybilles erste Flutter App'),
+      home: SybillesKleineSeite(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class SybillesKleineSeite extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SybillesKleineSeiteState createState() => _SybillesKleineSeiteState();
 }
 
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _SybillesKleineSeiteState extends State<SybillesKleineSeite> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Sybilles super Seite"),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Image.network("https://bilder.t-online.de/b/84/93/82/46/id_84938246/920/tid_da/eichhoernchen-geert-weggen-zeigt-die-welt-der-kleinen-nager-.jpg"),
+            machMirNeReihe("Name", "Sybille"),
+            machMirNeReihe("Alter", "21"),
+            machMirNeReihe("Hobby", "Fußball"),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget machMirNeReihe(attribut, wert) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text(
+            attribut,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            wert,
+            style: TextStyle(fontSize: 20),
+          ),
+        ],
+      ),
     );
   }
 }
